1e84cd46de3ff031091f53558676b9256abc3d6e
Philippe Turcotte <philsport003@hotmail.com>
2015-03-11 11:04:50 -0400

    All tests for point are passing.

--- Shape/point.cpp
+++ Shape/point.cpp
@@ -9,17 +9,23 @@ Point::Point( double _x, double _y ) :
 	x(_x), 
 	y(_y)
 {	
-	
+	if (_x < 0 || _y < 0)
+	{
+		throw invalid_argument("Coordonate can't be negative.");
+	}
 }
 
 bool Point::operator == (const Point & point) const
 {
-	throw logic_error("Not Implmented yet");
-	return true;
+	if (this->x == point.x && this->y == point.y)
+	{
+		return true;
+	}
+	return false;
 }
 
 bool Point::operator != (const Point & point) const
 {
-	throw logic_error("Not Implmented yet");
+	if (!(*this == point)) return true;
 	return false; 
 }
